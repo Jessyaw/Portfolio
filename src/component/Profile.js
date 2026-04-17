19ca022ffe21158576edf169f7bd5dfffeca5d18
JessyAngel07 <144873346+JessyAngel07@users.noreply.github.com>
2026-04-17 11:53:28 +0530

    jc

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -3,16 +3,16 @@ import { Color } from '../Colors'
 export default function Profile(props) {
     return (
         <div>
-            <div style={{ display: 'flex', flexDirection: 'row', gap: '25px', alignItems: 'center' }}>
+            <div style={{ display: 'flex', flexDirection: 'row', gap: '12px', alignItems: 'center' }}>
                 <div className='center'
                     style={{
-                        height: '50px', width: '50px', borderRadius: '25px', backgroundColor: props.backgroundColor ? props.backgroundColor : Color.libraryBG,
-                        fontWeight: 'bold', fontSize: '16px', color: props.color ? props.color : Color.blackFont,
+                        height: '40px', width: '40px', borderRadius: '20px', backgroundColor: props.backgroundColor ? props.backgroundColor : Color.libraryBG,
+                        fontWeight: 'bold', fontSize: '12px', color: props.color ? props.color : Color.blackFont,
                     }}>{props.name ? props.name.split(0, 1) : 'J'}
                 </div>
                 <div>
-                    <div style={{ fontWeight: 'bold', fontSize: '16px' }}>{props.name ? props.name : 'Jessy angel'}</div>
-                    <div style={{ color: Color.grey, fontSize: '12px' }}>{props.email ? props.email : 'jessy@itpro.com'}</div>
+                    <div style={{ fontWeight: 'bold', fontSize: '12px' }}>{props.name ? props.name : 'Jessy angel'}</div>
+                    <div style={{ color: Color.grey, fontSize: '10px' }}>{props.email ? props.email : 'jessy@itpro.com'}</div>
                 </div>
             </div >
         </div >
